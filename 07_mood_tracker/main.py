bf43b7294d0152f150d2108c7218a498829582b6
david7star7 <david7star7@gmail.com>
2025-04-16 21:45:31 +0500

    new changes

--- 07_mood_tracker/main.py
+++ 07_mood_tracker/main.py
@@ -66,7 +66,7 @@ if not data.empty:
     # Display bar chart of mood frequencies
     st.bar_chart(mood_counts)
 
-    # Build with love by Asharib Ali
-    st.write("Build with ❤️ by [Asharib Ali](https://github.com/AsharibAli)")
+    # Build with love by Muhammad Zeeshan
+    st.write("Build with ❤️ by [Muhammad Zeeshan](https://github.com/david7star7)")
 
     
